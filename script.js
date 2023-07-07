92c4a1f260dd76d63727edc4ff0842fd38148a4f
mondli16 <125990471+mondli16@users.noreply.github.com>
2023-07-07 12:51:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,8 @@
-let menu = document.querySelector('#menu');
-let main = document.querySelector('.main');
+const toggleIcon = document.querySelector('.toggle-icon');
 
-menu.onclick = () => {
-        menu.classList.toggle('bx-x');
-        main.classList.toggle('active'); 
-};
-
-      
-
-
-
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.addEventListener("scroll",function(){
-        var header = document.querySelector('header');
-        header.classList.toggle('sticky');
-
-
-        menu.classList.remove('bx-x');
-        main.classList.remove('active'); 
-        });
-
-window.onscroll= () => {
-        sections.forEach(sec=>{
-                let top = window.scrollY;
-                let offset = sec.offsetTop - 150;
-                let height = sec.offsetHeight;
-                let id = sec.getAttribute('id');
-
-                if(top >= offset && top < offset + height){
-                        navLinks.forEach(links => {
-                                links.classList.remove('active');
-                                document.querySelector('header nav a [href*='+ id +']').classList.add('active');
-
-                        });
-                };
-        });
-
-  
-}
-
-ScrollReveal({
-        reset: true,
-        distance: '80px',
-        duration: 2000,
-        delay: 200
-});
-
-ScrollReveal().reveal('.home-section, .heading', {origin:'top'});
-ScrollReveal().reveal('.home-image, .service-section,.port-box, .contact i', {origin:'bottom'});
-ScrollReveal().reveal('.home-section h1, .about-image', {origin:'left'});
-ScrollReveal().reveal('.home-section p, .about-section', {origin:'right'});
-
-
-const typed = new Typed('.text',{
-        strings:['Developer','Business Analyst'],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true
+toggleIcon.addEventListener('click', () => {
+    toggleIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark');
 });
 
 
