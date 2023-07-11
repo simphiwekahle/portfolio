664558e741c12781293cf19ee9af68397b03402d
mondli16 <125990471+mondli16@users.noreply.github.com>
2023-07-11 14:26:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
+let menuIcon = document.querySelector('#menuicon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}  
+
+// Select sections and navigation links
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+// Function to handle scroll event
+window.addEventListener('scroll', function() {
+  // Loop through sections
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    // Check if the section is in the viewport
+    if (top >= offset && top < offset + height) {
+      // Remove 'active' class from all navigation links
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+      });
+
+      // Add 'active' class to the corresponding navigation link
+      document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
+    }
+  });
+
+  // Make the header sticky
+  let header = document.querySelector('.header');
+  header.classList.toggle('sticky', window.scrollY > 100);
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active'); 
+
+
+});
+
 const toggleIcon = document.querySelector('.toggle-icon');
 
 toggleIcon.addEventListener('click', () => {
     toggleIcon.classList.toggle('bx-sun');
-    document.body.classList.toggle('dark');
+    document.body.classList.toggle('darkicon');
 });
 
-
 function readmore() {
-        var dots = document.getElementById("dots");
-        var moreText = document.getElementById("more");
-        var btnText = document.getElementById("button");
-      
-        if (dots.style.display === "none") {
-          dots.style.display = "inline";
-          btnText.innerHTML = "Read more";
-          moreText.style.display = "none";
-          
-        } else {
-          dots.style.display = "none";
-          btnText.innerHTML = "Read less";
-          moreText.style.display = "inline";
-        }
-      }
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("button");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+    
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
